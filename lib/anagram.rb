5dba627ad1b16dd26dd5023f6f78210cc104af60
Bria White <bria.awhite@gmail.com>
2020-08-09 03:58:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,11 +6,7 @@ class Anagram
     @word = word
   end
 
-  def match(word_array)
-    word_array.select do |word|
-      word.split("").sort == @word.split("").sort
-    end
-  end 
+  
 end   
 
 
